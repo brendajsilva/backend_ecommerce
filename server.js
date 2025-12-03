8fab5c86914611b5160cdc2f21b9ea05db5487ea
Brenda <brenda_j_silva@estudante.sesisenai.org.br>
2025-12-03 07:48:55 -0300

    versão corrigida

--- server.js
+++ server.js
@@ -7,7 +7,10 @@ const app = express()
 const PORT = process.env.PORT || 3000
 
 // Middlewares
-app.use(cors())
+app.use(cors({
+  origin: ['https://front-ecommerce-henna.vercel.app', 'http://localhost:3000'],
+  credentials: true
+}))
 app.use(express.json())
 app.use(express.urlencoded({ extended: true }))
 
